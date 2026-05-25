ceb08a7a2eb9540ec45e6806cbc02779b6eb12a9
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2026-05-26 06:03:54 +0900

    更新 main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,28 @@
+//% color="#000000" icon="\uf1e6" block="セレクトボックス付き拡張機能"
+namespace myExtensionWithSelectBox {
 
+    export enum MyOptions {
+        // % block="選択肢A"
+        OptionA = 1,
+        // % block="選択肢B"
+        OptionB = 2,
+        // % block="選択肢C"
+        OptionC = 3
+    }
 
-function hello () {
-	
+    // % block="コマンドを実行： %choice"
+    export function hello(choice: MyOptions): void {
+        // 選択された値に応じた処理
+        switch (choice) {
+            case MyOptions.OptionA: // オプションAの時の処理
+                basic.showString('a')
+                break;
+            case MyOptions.OptionB: // オプションBの時の処理
+                basic.showString('b')
+                break;
+            case MyOptions.OptionC: // オプションCの時の処理
+                basic.showString('c')
+                break;
+        }
+    }
 }
